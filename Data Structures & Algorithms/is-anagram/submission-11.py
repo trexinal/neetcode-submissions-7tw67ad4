6e21dcd4908003d5d1be91ccb5537e3c57fcb652
Ariel Moncrief <143503199+trexinal@users.noreply.github.com>
2026-03-31 17:57:46 -0700

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        seen = [0] * 26
+        for i in range(len(s)):
+            seen[ord(s[i]) - ord('a')] += 1
+            seen[ord(t[i]) - ord('a')] -= 1
+
+        for value in seen:
+            if value:
+                return False
+        return True
